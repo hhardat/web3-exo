9d5311de77c9744846a79ba4a66acd06978b28c3
hhardat <hugohardat@gmail.com>
2022-09-20 15:33:06 +0200

    module 2:events

--- exercises/module2/src/components/App/App.jsx
+++ exercises/module2/src/components/App/App.jsx
@@ -5,24 +5,25 @@ import { useState } from 'react'
 const App = () => {
     const [ counter, setCounter ] = useState(0)
   
-    const increaseByOne = () => setCounter(counter + 1)
-    const decreaseByOne = () => setCounter(counter - 1)
-    const setToZero = () => setCounter(0)
+    const changeCount = (delta) => setCounter(counter + parseInt(delta)) 
   
     return (
       <div>
         <Display counter={counter}/>
         <Button
-          onClick={increaseByOne}
+          onClick={changeCount}
           text='plus'
+          delta={1}
         />
         <Button
-          onClick={setToZero}
+          onClick={changeCount}
           text='zero'
+          delta={-counter}
         />     
         <Button
-          onClick={decreaseByOne}
+          onClick={changeCount}
           text='minus'
+          delta={-1}
         />           
       </div>
     )
--- exercises/module2/src/components/Button/Button.jsx
+++ exercises/module2/src/components/Button/Button.jsx
@@ -1,3 +1,8 @@
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text, delta }) => {
+
+    const handleClick = (e) => onClick(e.target.dataset.delta)
+
+    return <button data-delta={delta} onClick={handleClick}>{text}</button>
+}
 
 export default Button
