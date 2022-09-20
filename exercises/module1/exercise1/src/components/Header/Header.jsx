2587f65d590f9f00f2990547bf036e8812f043d1
hhardat <hugohardat@gmail.com>
2022-09-20 14:00:08 +0200

    1.1-1.2 : import image

--- exercises/module1/exercise1/src/components/Header/Header.jsx
+++ exercises/module1/exercise1/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
+import logo from './LOGO HE VINCI.png'
+
 const Header = (props) => {
     return(
       <div>
+        <img src={logo} alt="HE VINCI LOGO" />
         <h1>{props.course}</h1>
       </div>
     )
