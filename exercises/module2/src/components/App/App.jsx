0c9c0be90d8a088689211c22f15d20fea410c5bf
hhardat <hugohardat@gmail.com>
2022-09-20 15:21:23 +0200

    module 2: init

---
+++ exercises/module2/src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import Button from '../Button/Button'
+import Display from '../Display/Display'
+import { useState } from 'react'
+
+const App = () => {
+    const [ counter, setCounter ] = useState(0)
+  
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
+  
+    return (
+      <div>
+        <Display counter={counter}/>
+        <Button
+          onClick={increaseByOne}
+          text='plus'
+        />
+        <Button
+          onClick={setToZero}
+          text='zero'
+        />     
+        <Button
+          onClick={decreaseByOne}
+          text='minus'
+        />           
+      </div>
+    )
+  }
+
+export default App
